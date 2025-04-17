51edb10689c7292d2bdd8f1f00fc9713fb0bded4
Vaibhav Kumar <126766355+vaibhavkumar2001@users.noreply.github.com>
2025-04-17 13:27:41 +0530

    remove redundant comments and clean up code formatting in productExceptSelf implementation

--- ProductofArrayExceptSelf.cpp
+++ ProductofArrayExceptSelf.cpp
@@ -1,3 +1,12 @@
+// Based on the example above, it seems that by looping twice, once from the left and once from the right, the calculation can be simplified. The sum of the numbers to the left of an index can be calculated during the left-to-right loop, and the sum of the numbers to the right can be calculated during the right-to-left loop. This way, it seems possible to perform the calculation without skipping over the current index.
+
+
+// Yahan mera algorithm yeh h ki main pehle main output naam ka ek vector banayunga usko 1 se initialize ke doonga 
+
+// Fir main left ko left se loop start karoonga usme output ka 1 of index ko multiply kardoonga left ko 
+
+// fir main left ko multiply kardoonga nums[i] ko
+
 #include<iostream>
 #include<vector>
 using namespace std;
